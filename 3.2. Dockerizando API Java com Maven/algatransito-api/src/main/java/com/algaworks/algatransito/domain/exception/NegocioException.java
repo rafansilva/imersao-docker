da71a7355dc3ce6342d76c69210995c0f62b05c9
Rafael N. Silva <31461569+rafansilva@users.noreply.github.com>
2026-07-23 21:48:36 -0300

    .

---
+++ 3.2. Dockerizando API Java com Maven/algatransito-api/src/main/java/com/algaworks/algatransito/domain/exception/NegocioException.java
@@ -0,0 +1,7 @@
+package com.algaworks.algatransito.domain.exception;
+
+public class NegocioException extends RuntimeException {
+    public NegocioException(String message) {
+        super(message);
+    }
+}
---
+++ 3.2. Dockerizando API Java com Maven/algatransito-api/src/main/resources/db/migration/V003__cria-pk-para-veiculo.sql
@@ -0,0 +1,2 @@
+alter table veiculo add constraint pk_veiculo
+primary key(id)
